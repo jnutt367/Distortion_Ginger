d3a325d29bce770eaf6e0ac61b69c9e664884373
Jason Nutt <jnutt367@gmail.com>
2021-05-22 09:28:28 -0400

    commit for inspection

--- gsap.js
+++ gsap.js
@@ -1,3 +1,7 @@
+gsap.fromTo("body", { scale: 0 }, { duration: 3, scale: 2, x: 0, backgroundColor: "black" })
+gsap.to("body", { duration: 5, x: -100, scale: 0.5, opacity: 1, position: "relative", Zindex: 2 }, 2)
+gsap.to("p", { float: "left", marginRight: "500", color: "orange" })
+
 const square = document.querySelector('div');
 
 document.addEventListener('mousemove', (e) => {
